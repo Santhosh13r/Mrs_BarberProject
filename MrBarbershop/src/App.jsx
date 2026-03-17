0757edd546bb3626c9662bf0cfc4d3643bebe643
Baviravichandran <bavyasudha20@gmail.com>
2026-03-17 16:05:07 +0530

    new changes

--- MrBarbershop/src/App.jsx
+++ MrBarbershop/src/App.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
 
-function App() {
+function App(){
   const [count, setCount] = useState(0)
-=======
-import React from 'react'
-import { BrowserRouter, Routes } from 'react-router'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
->>>>>>> 20c26383a7bbc93d09f3f53448924d15370a0554
 
 const App = () => { 
   return (
     <>
-<<<<<<< HEAD
-      <Navbar></Navbar>
-=======
 
     <BrowserRouter>
     <Navbar/>
@@ -38,9 +25,10 @@ const App = () => {
     </BrowserRouter>
     
     
->>>>>>> 20c26383a7bbc93d09f3f53448924d15370a0554
     </>
   )
 }
 
+
+} 
 export default App
